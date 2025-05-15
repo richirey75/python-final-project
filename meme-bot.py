2e8d8b9b90d0e54d7fb2f42d49c68e52181f9052
Richard <richirey75@gmail.com>
2025-05-14 18:11:06 -0700

    connecting to discord api

---
+++ meme-bot.py
@@ -0,0 +1,11 @@
+import discord
+
+class MyClient(discord.Client):
+  async def on_ready(self):
+    print('Logged on as {0}!'.format(self.user))
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = MyClient(intents=intents)
+client.run('Your token goes here') # Replace with your own token.
